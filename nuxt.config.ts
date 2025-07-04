08f64282d4aa94f00c0ff72272c1ab6e5dd5dd8f
Guillermo Farías <guillermodanilo08@gmail.com>
2025-07-03 21:21:36 -0400

    add seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,13 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', href: '/favicon-light.png', media: '(prefers-color-scheme: light)' },
         { rel: 'icon', href: '/favicon-dark.png', media: '(prefers-color-scheme: dark)' },
-      ]
+      ],
+      title: 'Guillermo Farías - Personal Website',
+      meta: [
+        { name: 'description', content: 'Senior Software Engineer with a passion for building scalable web applications.' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: 'utf-8' }
+      ],
     }
   },
   css: ['~/assets/css/main.css'],
